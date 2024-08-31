17e30758a37d2813fecbbf1d6bf1ccdcf86c10f5
Den <treshe001@gmail.com>
2024-08-31 23:36:03 +0300

    final_mayb122

--- form.js
+++ form.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
         // const digitsOnly = phoneValue.JSON.stringify();
         const phonePattern = /^\+38 \((067|068|096|097|098|050|066|095|099|063|073|093|091)\) \d{3} \d{4}$/; // Регулярний вираз для номера телефону
 
-        console.log(digitsOnly);
         if (!phonePattern.test(phoneInput.val())) {
             event.preventDefault(); // Заборонити надсилання форми
             phoneInput.css('border', '1px solid red'); // Змінити бордер на червоний
